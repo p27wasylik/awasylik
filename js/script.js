492a710306dd9379f01de0b80e883d1c5bf943d9
Adrian Wasylik <p27wasylik@gmail.com>
2022-10-18 21:40:55 +0200

    added function to js file, rebiuld nav with flex

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
-let menuAboutMe = document.querySelector(".js-buttonMenuAboutMe");
-let menuHobby = document.querySelector(".js-buttonMenuHobby");
-let menuPortfolio = document.querySelector(".js-buttonMenuPortfolio");
+{
+    const menuAboutMe = document.querySelector(".js-buttonMenuAboutMe");
+    const menuHobby = document.querySelector(".js-buttonMenuHobby");
+    const menuPortfolio = document.querySelector(".js-buttonMenuPortfolio");
 
-let aboutMeElement = document.querySelector(".js-aboutMe");
-let hobbyElement = document.querySelector(".js-hobby");
-let portfolioElement = document.querySelector(".js-portfolio");
+    const aboutMeElement = document.querySelector(".js-aboutMe");
+    const hobbyElement = document.querySelector(".js-hobby");
+    const portfolioElement = document.querySelector(".js-portfolio");
 
-menuAboutMe.addEventListener("click", () => {
-    aboutMeElement.classList.remove("section--hidden");
-    hobbyElement.classList.add("section--hidden");
-    portfolioElement.classList.add("section--hidden");
-});
+    const sectionHiddenContens = (sectionElementHidden) => {
+        sectionElementHidden.classList.add("section--hidden");
+    }
 
-menuHobby.addEventListener("click", () => {
-    hobbyElement.classList.remove("section--hidden");
-    aboutMeElement.classList.add("section--hidden");
-    portfolioElement.classList.add("section--hidden");
-});
+    const sectionContens = (sectionElement, sectionElementHidden, sectionElementHidden2) => {
+        sectionElement.classList.remove("section--hidden");
+        sectionHiddenContens(sectionElementHidden);
+        sectionHiddenContens(sectionElementHidden2);
+    }
 
-menuPortfolio.addEventListener("click", () => {
-    portfolioElement.classList.remove("section--hidden");
-    aboutMeElement.classList.add("section--hidden");
-    hobbyElement.classList.add("section--hidden");
-});
+    menuAboutMe.addEventListener("click", () => {
+        sectionContens(aboutMeElement, hobbyElement, portfolioElement);
+    });
+
+    menuHobby.addEventListener("click", () => {
+        sectionContens(hobbyElement, aboutMeElement, portfolioElement);
+    });
+
+    menuPortfolio.addEventListener("click", () => {
+        sectionContens(portfolioElement, aboutMeElement, hobbyElement);
+    });
+    
+}
